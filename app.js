f6cca94ead94da56f135361f265f7a96c93aba55
hamza <hamazrauf@gmail.com>
2023-09-08 17:58:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const db = require("./helper/db.js");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth.js");
+const app = express();
+const socketHelper = require("./helper/socket.js");
+const messageRoutes = require("./routes/message.js");
+const cors = require("cors");
+const connectedUsers = new Set();
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your frontend URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // This allows cookies to be sent from the frontend
+  optionsSuccessStatus: 204, // No content response for preflight requests
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(messageRoutes);
+
+const startServer = async () => {
+  try {
+    const server = app.listen(process.env.PORT);
+    socketHelper.initSocket(server);
+    const io = socketHelper.getIo();
+    io.on("connection", (socket) => {
+      const username = socket.handshake.query.username;
+
+      connectedUsers.add(username); // Add user with both ID and username
+
+      // Remove the user from the set when they disconnect
+      socket.on("disconnect", () => {
+        connectedUsers.delete(username);
+
+        io.emit("userList", Array.from(connectedUsers));
+      });
+
+      // Send an updated list of connected users to all clients
+      io.emit("userList", Array.from(connectedUsers));
+
+      // You can also log the connected users at this point
+    });
+
+    // After the server has started listening, establish the MongoDB connection
+    await db.makeConnection();
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+startServer();
